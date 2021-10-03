2dd8c9e0fe4683023741ef084f8a431a2528b9d7
arpieb <rob@octobang.com>
2021-10-03 15:36:34 -0400

    Added NLTEx.WordVectors.index_tokens

--- lib/nlt_ex/word_vectors.ex
+++ lib/nlt_ex/word_vectors.ex
@@ -15,7 +15,7 @@ defmodule NLTEx.WordVectors do
   end
 
   @doc ~S"""
-  Vectorize a list of tokens using the provided word vector mappings
+  Vectorize a list of tokens into a list of n-dimensional vectors using the provided word vector mappings
 
   ## Examples
 
@@ -32,5 +32,24 @@ defmodule NLTEx.WordVectors do
     tokens
     |> Enum.filter(fn t -> Map.has_key?(words, t) end)
     |> Enum.map(fn t -> vectors[Map.get(words, t)] end)
+  end
+
+  @doc ~S"""
+  Vectorize a list of tokens into a list of word-vector indices using the provided ordered word vector vocabulary
+
+  ## Examples
+
+      iex> tokens = String.split("the fat cat")
+      iex> {words, _} = tokens |> Enum.reduce({%{}, 0}, fn w, {v, i} -> {Map.put(v, w, i), i + 1} end)
+      iex> vecs = Nx.eye(length(Map.keys(words)))
+      iex> wv = NLTEx.WordVectors.new(words, vecs)
+      iex> [0, 1, 2] = NLTEx.WordVectors.index_tokens(tokens, wv)
+  """
+  def index_tokens(tokens, %__MODULE__{} = wv) do
+    words = wv.words
+
+    tokens
+    |> Enum.filter(fn t -> Map.has_key?(words, t) end)
+    |> Enum.map(fn t -> Map.get(words, t) end)
   end
 end
